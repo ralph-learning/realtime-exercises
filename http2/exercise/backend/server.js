a59fb2c4c3852792170d75932bc7b96a55235c73
Ralph Effting <ralfting@gmail.com>
2024-01-09 08:01:41 -0300

    Implementing http2 push connection

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -29,11 +29,30 @@ const server = http2.createSecureServer({
   key: fs.readFileSync(path.join(__dirname, "/../key.pem")),
 });
 
-/*
- *
- * Code goes here
- *
- */
+server.on("stream", (stream, headers) => {
+  const method = headers[":method"];
+  const path = headers[":path"];
+
+  // streams will open for everything, we want just GETs on /msgs
+  if (path === "/msgs" && method === "GET") {
+    // immediately respond with 200 OK and encoding
+    stream.respond({
+      ":status": 200,
+      "content-type": "text/plain; charset=utf-8",
+    });
+
+    // write the first response
+    stream.write(JSON.stringify({ msgs: getMsgs() }));
+
+    // keep track of the connection
+    connections.push(stream);
+
+    // when the connection closes, stop keeping track of it
+    stream.on("close", () => {
+      connections = connections.filter((s) => s !== stream);
+    });
+  }
+});
 
 server.on("request", async (req, res) => {
   const path = req.headers[":path"];
@@ -53,11 +72,17 @@ server.on("request", async (req, res) => {
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    msg.push({
+      user,
+      text,
+      time: Date.now(),
+    });
+
+    res.end();
+
+    connections.forEach((stream) => {
+      stream.write(JSON.stringify({ msgs: getMsgs() }));
+    });
   }
 });
 
